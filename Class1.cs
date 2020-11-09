389d92cb59aa2e56cdd0a14947314100a6d8f117
Nadya <nadya2001chuprova5@gmail.com>
2020-11-10 01:03:30 +0300

    Homework 2

---
+++ Class1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2._1
+{
+    class Class1
+    {
+        public void Print(int value)
+        {
+            Console.WriteLine($"{value}");
+        }
+
+        public int MinOfThree(int a, int b, int c)
+        {
+            int min;
+            if ((a < b) && (a < c))
+                min = a;
+            else if ((b < a) && (b < c))
+                min = b;
+            //if ((c < a) && (c < b))
+            else
+                min = c;
+
+            return min;
+        }
+
+        public int CountOfNumbers(int number)
+        {
+            int k = 0;
+            while (number != 0)
+            {
+                number = number / 10;
+                k = k + 1;
+            }
+            return k;
+        }
+
+        public int Sum(int ch)
+        {
+            if ((ch % 2 != 0) && (ch > 0))
+            {
+                return ch;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool CheckLogin(string login)
+        {
+            bool accessL = false;
+            string loginR = "root";
+            if (loginR == login)
+                accessL = true;
+
+            else
+                accessL = false;
+
+            return accessL;
+        }
+
+        public bool CheckPassword(string password)
+        {
+            bool accessP = false;
+            string passwordR = "GeekBrains";
+            if (passwordR == password)
+                accessP = true;
+
+            else
+                accessP = false;
+
+            return accessP;
+        }
+                
+        public double CheckFat(double h, double m)
+        {
+            double I;
+            I = m / (h* h);
+            return I;
+        }
+        public double CheckI(double I, double h)
+        {
+            double rightI;
+            double rightH;
+            if (I < 18.5)
+            {
+                Console.WriteLine($"IMT= {I:0.00}");
+                Console.WriteLine("Слишком маленький вес ");
+                rightI = 18.5 - I;
+                rightH = rightI * h * h;
+                Console.WriteLine($"Вам нужно набрать {rightH:0.00} кг");
+            }
+
+
+            else if ((I >= 18.5) && (I < 25))
+            {
+                Console.WriteLine($"IMT= {I:0.00}");
+                Console.WriteLine("Нормальный вес ");
+            }
+
+
+            else if (I >= 25)
+            {
+                Console.WriteLine($"IMT= {I:0.00}");
+                Console.WriteLine("Слишком большой вес ");
+                rightI = I - 25;
+                rightH = rightI * h * h;
+                Console.WriteLine($"Вам нужно похудеть на {rightH:0.00} кг");
+            }
+            return 0;
+        }
+
+        public int CountN()
+        {
+            DateTime start = DateTime.Now;
+            int k = 0;
+            int i;
+            
+            for (i = 1; i < 1000000000; i++)
+            {
+                int sum = 0;
+                int j = i;
+                do
+                {
+                    //sum = 0;
+                    sum = sum + (j % 10);
+                    j = j / 10;
+                }
+                while (j != 0);
+
+                if (i % sum == 0)
+                    k++;
+            }
+            DateTime finish = DateTime.Now;
+            double time = (finish - start).TotalSeconds;
+            Console.WriteLine($"Время, потраченное на подсчет, равно {time}");
+            return k;
+        }
+
+        public void RecurF(int a, int b, int summ)
+        {
+            //Console.WriteLine($"\n");
+            if (a <= b)
+            {
+                Console.WriteLine($"{a} ");
+                summ = summ + a;
+                a++;
+                RecurF(a, b, summ);
+            }
+            else if (a == b+1)
+                Console.WriteLine($"\nСумма чисел равна {summ}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+
+            //Нахождение минимального из трех чисел
+             a.Print(a.MinOfThree(5, 2, 3));
+             Console.WriteLine("\n");
+
+             //Нахождение количества цифр в числе
+             a.Print(a.CountOfNumbers(1234463));
+             Console.WriteLine("\n");
+
+             //Подсчет количества нечетных пложительных чисел
+             Console.WriteLine("Подсчет количества нечетных пложительных чисел\n");
+             int ch = 1;
+             int sum = 0;
+             while(ch != 0)
+             {
+                 Console.WriteLine("Введите число, если хотите закончить - введите ноль ");
+                 ch =Convert.ToInt32(Console.ReadLine());
+                 sum = sum + a.Sum(ch);
+             }
+             Console.WriteLine("Сумма положительных нечетных чисел равна: ");
+             a.Print(sum);
+             Console.WriteLine("\n");
+
+             //Проверка логина и пароля
+             int tr = 0;
+             bool accessL;
+             string login;
+             do
+             {
+                 Console.WriteLine($"Введите логин, осталось {3-tr} попыток: ");
+                 login = Console.ReadLine();
+                 tr++;                
+                 accessL = a.CheckLogin(login);
+
+                 if (accessL == true)
+                 {
+                     Console.WriteLine("Логин введен правильно ");
+                     break;
+                 }
+             }
+             while (tr != 3);
+
+             if (accessL == false)
+                 Console.WriteLine("Доступ запрещен");
+
+             else
+             {
+                 tr = 0;
+                 bool accessP;
+                 string password;
+                 do
+                 {
+                     Console.WriteLine($"Введите пароль, осталось {3 - tr} попыток: ");
+                     password = Console.ReadLine();
+                     accessP = a.CheckPassword(password);
+                     tr++;
+                     if (accessP == true)
+                     {
+                         Console.WriteLine("Логин введен правильно ");
+                         break;
+                     }
+                 }
+                 while (tr != 3);
+
+                 if (accessP == false)
+                     Console.WriteLine("Доступ запрещен");
+             }
+             Console.WriteLine("\n");
+
+             //Подсчет индекса массы тела
+             Console.WriteLine("Подсчет индекса массы тела\n");
+             Console.WriteLine("Введите Ваш рост");
+             double h = Convert.ToDouble(Console.ReadLine()) / 100;
+
+             Console.WriteLine("Введите Ваш вес ");
+             double m = Convert.ToDouble(Console.ReadLine());
+
+             double I = a.CheckFat(h, m);
+             a.CheckI(I, h);
+             Console.WriteLine("\n");
+
+             //Подсчет "Хороших" чисел
+             int k;
+             k = a.CountN();
+             Console.WriteLine($"Количество Хороших чисел равно {k}");
+             Console.WriteLine("\n");
+            
+            //Рекурсивный вывод чисел
+            Console.WriteLine("Рекурсивный вывод чисел\n");
+
+            Console.WriteLine("Введите первое число ");
+            int first= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            
+            int summ = 0;
+
+            a.RecurF(first, second, summ);
+            
+            Console.ReadKey();    
+        }
+    }
+}
